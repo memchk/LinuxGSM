2cf2a9acfedb52c0f6cb7a9b520f7e5367e93f9b
Carson Page <carson.page@byterealm.com>
2019-05-25 18:18:13 -0500

    setup client/server divide fixes

--- lgsm/functions/command_armamods.sh
+++ lgsm/functions/command_armamods.sh
@@ -11,21 +11,24 @@ local function_selfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 check.sh
 
-# Create a temporary file to store the steamcmd script
-scriptfile="$(mktemp)"
 
-echo "DepotDownloadProgressTimeout 6000
-force_install_dir ${serverfiles}
-login ${steamuser} ${steampass}" >> $scriptfile
+if [ "$2" != "skipdl"]; then
+    # Create a temporary file to store the steamcmd script
+    scriptfile="$(mktemp)"
 
-for mod in "${workshopmods[@]}"; do
-    modid=$(echo "$mod" | awk '{print $2}')
-    echo "workshop_download_item 107410 ${modid} validate" >> $scriptfile
-done
+    echo "DepotDownloadProgressTimeout 6000
+    force_install_dir ${serverfiles}
+    login ${steamuser} ${steampass}" >> $scriptfile
 
-echo "quit" >> $scriptfile
+    for mod in "${workshopmods[@]}"; do
+        modid=$(echo "$mod" | awk '{print $2}')
+        echo "workshop_download_item 107410 ${modid} validate" >> $scriptfile
+    done
 
-${steamcmddir}/steamcmd.sh +runscript $scriptfile 
+    echo "quit" >> $scriptfile
+
+    ${steamcmddir}/steamcmd.sh +runscript $scriptfile 
+fi
 
 # Create mods folder if it does not exist, clear existing symlinks
 mkdir -p "$serverfiles/mods"
@@ -62,7 +65,7 @@ for mod in "${workshopmods[@]}"; do
     modtype=$(echo "$mod" | awk '{print $3}')
 
     if [ "$modtype" != "server" ]; then
-        find "$serverfiles/mods/@${modname}/" -type f -name "*.bikey" -exec ln -sf {} "$serverfiles/keys/" \;
+        find "$serverfiles/steamapps/workshop/content/107410/$modid" -type f -name "*.bikey" -exec ln -sf {} "$serverfiles/keys/" \;
     fi
 done
 
