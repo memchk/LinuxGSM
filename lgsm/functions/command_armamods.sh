093ae713f079f4a2bd29cedd511e5e0d69a091b6
Carson Page <carson.page@byterealm.com>
2019-05-25 13:57:51 -0500

    Print mods correctly fo real

--- lgsm/functions/command_armamods.sh
+++ lgsm/functions/command_armamods.sh
@@ -33,7 +33,7 @@ ${steamcmddir}/steamcmd.sh +runscript $scriptfile
 mkdir -p "$serverfiles/mods"
 find "$serverfiles/mods/" -maxdepth 1 -type l -delete
 
-modstring='mods="'
+modstring="mods=\""
 
 for mod in "${workshopmods[@]}"; do
     modname=$(echo "$mod" | awk '{print $1}')
